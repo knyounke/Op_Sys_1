7388219cac87f2bdc77176c735a412b8e66deda0
katie <knyounke@gmail.com>
2018-01-29 23:27:58 -0600

    fixed error with input on cin

--- shell.cpp
+++ shell.cpp
@@ -15,45 +15,56 @@ int main(void){
    pid_t child; //process id
    DIR * directory;
    struct dirent * directory_entry;
-   int i, j, k; //incrementors
+   int i, j;
+   string k;
    string buffer, command; //buffers for command and string
    time_t time;
 
    while(true){
 	cout<< "Time: "<< ctime(&time) << endl;
-	buffer = get_current_dir_name();//figure out second argument size
+	buffer = get_current_dir_name(); //handles unknown string size instead of char[]
   	 cout << endl << "Current Directory: " << buffer << endl;
 	
 	directory = opendir(".");
 	j = 0;
 	while((directory_entry = readdir(directory))){
 		if((directory_entry->d_type) & DT_DIR)
-			cout << j << "Directory: " << directory_entry->d_name << endl;
+			cout << j << " Directory: " << directory_entry->d_name << endl;
+		j++;
+
 		if( ( j % 8 ) == 0) {
-		  cout << "Hit N for Next" << endl; //handle times if only subdirs (if none, goes in infinite loop)
+		  cout << "Hit N for Next" << endl; 
 		  cin >> k;
 		}
 	}
 	closedir(directory);
 	cout << "------------------------------------" << endl;
-	cin >> j;
-	switch (j) {
-	 case 'q': exit(0);
-		   break;
-	 case 'e': cout << "Edit What? " << endl;
+	getline(cin, k);
+
+
+	if(k == "q")
+	  exit(0);
+	else if(k == "e"){
+	 cout << "Edit What? " << endl;
 		   cin >> buffer;
 		   command = "pico ";
 		   command += buffer;
 		   system(command.c_str());
-		   break;
-	 case 'r': cout << "Run what? " << endl;
+		
+		}
+	 else if(k == "r"){
+		   cout << "Run what? " << endl;
 		   cin >> command;
 		   system(command.c_str());
-		   break;
-	 case 'c': cout << "Change to? " << endl;
+		   }
+	 else if(k == "c"){
+	 	 cout << "Change to? " << endl;
 		   cin >> command;
 		   chdir(command.c_str());
-		   break;
+		   }
+	else
+		{
+		cout << "ERROR. Incorrect entry!" << endl << endl;
 	}
 	
 	
